14fa172374c54941d78aa83ffcea06007838fbd7
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2024-02-24 09:58:16 -0800

    feat(frontend): Adding Maps

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,14 +2,14 @@ import { Routes, Route } from 'react-router-dom'
 import NotFound from '@/components/NotFound'
 import Dashboard from '@/components/Dashboard'
 import DataTable from '@/components/DataTable'
-//import MapComponent from '@/components/MapView'
+import LeafletMapWithPoint from '@/components/MapView'
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/table" element={<DataTable />} />
-      {/*<Route path="/map" element={<MapComponent />} />*/}
+      <Route path="/map" element={<LeafletMapWithPoint />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
