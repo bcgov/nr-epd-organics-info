4c517e7b0e240d5fca69671074e96eb5925d2a66
Om Mishra <32200996+mishraomp@users.noreply.github.com>
2024-02-24 21:23:24 -0800

    feat: map updates (#32)

--- frontend/src/__tests__/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import App from '@/App'
-import { render, screen } from '../test-utils'
-
-describe('Simple working test', () => {
-  it('the title is visible', () => {
-    render(<App />)
-  })
-})
