2e034707385805d7cada6cef9c4b19e6ca80cd16
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2024-03-25 16:03:48 -0700

    header and pin changes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -18,35 +18,66 @@ export default function Header() {
   return (
     <AppBar position="fixed" sx={styles.appBar}>
       <Toolbar
-        sx={{ flexDirection: 'row', alignItems: 'center', alignContent: 'center', justifyContent: 'space-between' }}
-        id="back-to-top-anchor">
+        sx={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          alignContent: 'center',
+          justifyContent: 'space-between',
+        }}
+        id="back-to-top-anchor"
+      >
         <Stack direction="row" id="logo_name">
           <a href="/">
             <img alt="Logo" src={BCGovLogo} />
           </a>
-          <div>  <span style={{
-            fontFamily: 'BCSans', fontSize: '1.5em',
-            lineHeight: '2.25em', marginLeft: '1em', alignItems: 'center',
-          }}>
-            Organics Info
-          </span></div>
+          <div>
+            {' '}
+            <span
+              style={{
+                fontFamily: 'BCSans',
+                fontSize: '1.5em',
+                lineHeight: '2.25em',
+                marginLeft: '1em',
+                alignItems: 'center',
+              }}
+            >
+              Organics Info
+            </span>
+          </div>
         </Stack>
         <Stack direction="row" id="nav">
-          {mdMatches && <div>
-            <a href={'/search'} target={'_self'}>
-              <IconButton>
-                <Feed color="secondary"></Feed>
-              </IconButton>
-              <span style={{ color: '#ffffff', textDecoration: 'none' }}> Text Search</span>
-            </a>
-          </div>}
-          {!mdMatches && <div><a href={'/search'} target={'_self'}>
-            <IconButton>
-              <Menu color="secondary"></Menu>
-            </IconButton>
-          </a>
-          </div>
-          }
+          {mdMatches && (
+            <div>
+              <a
+                href={'/search'}
+                target={'_self'}
+                style={{ textDecoration: 'none' }}
+              >
+                <IconButton>
+                  <Feed color="secondary"></Feed>
+                </IconButton>
+                <span
+                  style={{
+                    color: '#ffffff',
+                    textDecoration: 'none',
+                    textTransform: 'none',
+                  }}
+                >
+                  {' '}
+                  Text Search
+                </span>
+              </a>
+            </div>
+          )}
+          {!mdMatches && (
+            <div>
+              <a href={'/search'} target={'_self'}>
+                <IconButton>
+                  <Menu color="secondary"></Menu>
+                </IconButton>
+              </a>
+            </div>
+          )}
         </Stack>
       </Toolbar>
     </AppBar>
