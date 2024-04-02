fed575843a0f5183811ae516e5dc798c8ef9ce67
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2024-04-02 09:32:02 -0700

    fix: remove prisma related stuff from code as there is no DB at this moment.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config'
-import { Logger, MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common'
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common'
 import { HTTPLoggerMiddleware } from './middleware/req.res.logger'
-import { loggingMiddleware, PrismaModule } from 'nestjs-prisma'
 import { ConfigModule } from '@nestjs/config'
 import { AppService } from './app.service'
 import { AppController } from './app.controller'
@@ -14,41 +13,10 @@ import { ObjectStoreService } from './v1/object-store/object.store.service'
 import { TasksModule } from './v1/tasks/task.module'
 import { ScheduleModule } from '@nestjs/schedule'
 
-const DB_HOST = process.env.POSTGRES_HOST || 'localhost'
-const DB_USER = process.env.POSTGRES_USER || 'postgres'
-const DB_PWD = encodeURIComponent(process.env.POSTGRES_PASSWORD || 'default') // this needs to be encoded, if the password contains special characters it will break connection string.
-const DB_PORT = process.env.POSTGRES_PORT || 5432
-const DB_NAME = process.env.POSTGRES_DATABASE || 'postgres'
-const DB_SCHEMA = process.env.DB_SCHEMA || 'users'
-
-function getMiddlewares() {
-  if (process.env.PRISMA_LOGGING) {
-    return [
-      // configure your prisma middleware
-      loggingMiddleware({
-        logger: new Logger('PrismaMiddleware'),
-        logLevel: 'debug',
-      }),
-    ]
-  }
-  return []
-}
-
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TerminusModule,
-    PrismaModule.forRoot({
-      isGlobal: true,
-      prismaServiceOptions: {
-        prismaOptions: {
-          log: ['error', 'warn'],
-          errorFormat: 'pretty',
-          datasourceUrl: `postgresql://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?schema=${DB_SCHEMA}&connection_limit=5`,
-        },
-        middlewares: getMiddlewares(),
-      },
-    }),
     ThrottlerModule.forRoot([{
       ttl: 60000,
       limit: 1000,
