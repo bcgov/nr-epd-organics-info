133f83b9422dceb994f130b2f434ed4cf6519ec6
Om <omprakashmishra3978@gmail.com>
2024-05-23 12:22:10 -0700

    fix failing UTs

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -2,14 +2,17 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ObjectStoreService } from "./v1/object-store/object.store.service";
+import {AmsOracleConnectorService} from "./v1/ams-oracle-connector/ams.oracle.connector.service";
+import {HttpModule} from "@nestjs/axios";
 
 describe("AppController", () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       controllers: [AppController],
-      providers: [AppService, ObjectStoreService],
+      providers: [AppService, ObjectStoreService, AmsOracleConnectorService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
