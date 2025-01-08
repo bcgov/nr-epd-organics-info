a2c3b1b6e7f693e6a8d98e28ce1f94cf9d07b05b
Jamie Popkin <popkinj@littleearth.ca>
2025-01-08 13:20:05 -0800

    Working as expected now

--- frontend/src/pages/map/layers/BasemapControl.tsx
+++ frontend/src/pages/map/layers/BasemapControl.tsx
@@ -15,51 +15,67 @@ export function BasemapControl() {
   const map = useMap()
 
   useEffect(() => {
-    const switcher = new L.basemapsSwitcher(
-      [
-        {
-          layer: L.tileLayer(
-            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            {
-              attribution:
-                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            },
-          ), //DEFAULT MAP
-          icon: './assets/images/img1.PNG',
-          name: 'Map one',
-        },
-        {
-          layer: L.tileLayer(
-            'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png',
-            {
-              attribution:
-                '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-            },
-          ).addTo(map),
-          icon: './assets/images/img2.PNG',
-          name: 'Map two',
-        },
-        {
-          layer: L.tileLayer(
-            'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
-            {
-              attribution:
-                'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
-            },
-          ),
-          icon: './assets/images/img3.PNG',
-          name: 'Map three',
-        },
-      ],
-      { position: 'bottomright' },
-    ).addTo(map)
-    console.log('basemap control', switcher)
+    // Keep track of the current active layer
+    let activeLayer: L.TileLayer | null = null
 
-    // Cleanup function to remove the control when component unmounts
+    const basemaps = [
+      {
+        layer: L.tileLayer(
+          'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          {
+            attribution:
+              '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          },
+        ),
+        icon: './assets/images/img1.PNG',
+        name: 'Map one',
+      },
+      {
+        layer: L.tileLayer(
+          'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png',
+          {
+            attribution:
+              '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>...',
+          },
+        ),
+        icon: './assets/images/img2.PNG',
+        name: 'Map two',
+      },
+      {
+        layer: L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+          attribution:
+            'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>...',
+        }),
+        icon: './assets/images/img3.PNG',
+        name: 'Map three',
+      },
+    ]
+
+    // Set initial active layer
+    activeLayer = basemaps[0].layer
+    activeLayer.addTo(map)
+
+    // Add event listener to handle layer changes
+    map.on('baselayerchange', (e: any) => {
+      if (activeLayer) {
+        map.removeLayer(activeLayer)
+      }
+      activeLayer = e.layer
+    })
+
+    const switcher = new L.basemapsSwitcher(basemaps, {
+      position: 'bottomright',
+    }).addTo(map)
+
+    // Cleanup
     return () => {
+      if (activeLayer) {
+        map.removeLayer(activeLayer)
+      }
       map.removeControl(switcher as unknown as L.Control)
+      map.off('baselayerchange')
     }
-  }, [map]) // Only run effect when map instance changes
+  }, [map])
 
   return null
 }
