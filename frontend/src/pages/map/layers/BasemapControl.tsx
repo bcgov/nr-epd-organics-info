633bf2b1035fc1cfcf0b9d5ae896e5f47c1341f3
Jamie Popkin <popkinj@littleearth.ca>
2025-01-09 12:38:38 -0800

    Different cors setting

--- frontend/src/pages/map/layers/BasemapControl.tsx
+++ frontend/src/pages/map/layers/BasemapControl.tsx
@@ -39,7 +39,7 @@ export function BasemapControl() {
           'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
           {
             attribution: 'Tiles &copy; Esri',
-            crossOrigin: true,
+            crossOrigin: 'anonymous',
           },
         ),
         icon: './imagery.png',
@@ -50,7 +50,7 @@ export function BasemapControl() {
           'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
           {
             attribution: 'Tiles &copy; Esri',
-            crossOrigin: true,
+            crossOrigin: 'use-credentials',
           },
         ),
         icon: './streets.png',
