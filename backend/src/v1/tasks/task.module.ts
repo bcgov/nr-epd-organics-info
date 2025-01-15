21e3fb15cfd7b50cbb7b7bda5df838d98dc12bb4
Om Mishra <32200996+mishraomp@users.noreply.github.com>
2025-01-14 16:37:01 -0800

    chore: remove some of the old code which are not being used, regular house keeping (#347)

--- backend/src/v1/tasks/task.module.ts
+++ backend/src/v1/tasks/task.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './task.service';
-import { ObjectStoreModule } from '../object-store/object.store.module';
 import { HttpModule } from '@nestjs/axios';
 import { AmsOracleConnectorModule } from '../ams-oracle-connector/ams.oracle.connector.module';
 
 @Module({
-  imports: [ObjectStoreModule, HttpModule, AmsOracleConnectorModule],
-  providers: [TasksService]
+  imports: [HttpModule, AmsOracleConnectorModule],
+  providers: [TasksService],
 })
-export class TasksModule {
-}
+export class TasksModule {}
