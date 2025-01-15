ab3939508b8313a1c9b58155c81c2b420f68102c
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2025-01-14 16:24:48 -0800

    chore: remove some of the old code which are not being used, regular house keeping

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,8 +8,6 @@ import { MetricsController } from './metrics.controller';
 import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from './health.controller';
 import { ThrottlerModule } from '@nestjs/throttler';
-import { ObjectStoreModule } from './v1/object-store/object.store.module';
-import { ObjectStoreService } from './v1/object-store/object.store.service';
 import { TasksModule } from './v1/tasks/task.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AmsOracleConnectorModule } from './v1/ams-oracle-connector/ams.oracle.connector.module';
@@ -21,23 +19,31 @@ import { HttpModule } from '@nestjs/axios';
     HttpModule,
     ConfigModule.forRoot(),
     TerminusModule,
-    ThrottlerModule.forRoot([{
-      ttl: 60000,
-      limit: 1000
-    }]),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 1000,
+      },
+    ]),
     ScheduleModule.forRoot(),
     TasksModule,
-    ObjectStoreModule,
-    AmsOracleConnectorModule
+    AmsOracleConnectorModule,
   ],
   controllers: [AppController, MetricsController, HealthController],
-  providers: [AppService, ObjectStoreService, AmsOracleConnectorService]
+  providers: [AppService, AmsOracleConnectorService],
 })
-export class AppModule { // let's add a middleware on all routes
+export class AppModule {
+  // let's add a middleware on all routes
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(HTTPLoggerMiddleware).exclude({ path: 'metrics', method: RequestMethod.ALL }, {
-      path: 'health',
-      method: RequestMethod.ALL
-    }).forRoutes('*');
+    consumer
+      .apply(HTTPLoggerMiddleware)
+      .exclude(
+        { path: 'metrics', method: RequestMethod.ALL },
+        {
+          path: 'health',
+          method: RequestMethod.ALL,
+        },
+      )
+      .forRoutes('*');
   }
 }
