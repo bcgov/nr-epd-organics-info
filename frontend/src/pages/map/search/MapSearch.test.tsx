ac5425a0fda83cc53035372be5acbeab99db8b38
Jamie Popkin <popkinj@littleearth.ca>
2025-01-28 12:58:02 -0800

    Missed this test

--- frontend/src/pages/map/search/MapSearch.test.tsx
+++ frontend/src/pages/map/search/MapSearch.test.tsx
@@ -37,7 +37,7 @@ describe('Test suite for MapSearch', () => {
       screen.queryByRole('button', { name: 'Layers' }),
     ).not.toBeInTheDocument()
     screen.getByRole('button', { name: 'Polygon Search' })
-    screen.getByRole('button', { name: 'Point Search' })
+    screen.getByRole('button', { name: 'Radius Search' })
     screen.getByText('Status')
     screen.getByRole('button', { name: 'Filter' })
   })
