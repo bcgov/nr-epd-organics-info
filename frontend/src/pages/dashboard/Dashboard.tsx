d0bdbee1c1470b85e321fa150ad4e924aaf439c0
Jamie Popkin <popkinj@littleearth.ca>
2025-02-05 12:06:27 -0800

    New component for this bit

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -3,12 +3,14 @@ import { Stack } from '@mui/material'
 import { InfoSection } from './InfoSection'
 import { LearnMoreSection } from './LearnMoreSection'
 import { TopSection } from './TopSection'
+import { UseThisTool } from './UseThisTool'
 
 export default function Dashboard() {
   return (
     <Stack direction="column" sx={{ marginTop: '4em' }}>
       <TopSection />
       <InfoSection />
+      <UseThisTool />
       <LearnMoreSection />
     </Stack>
   )
